81486303282e0862d90cc61f7f1ff5a121555a1f
B. Lokanath prasad das <45465660+loknathD@users.noreply.github.com>
2023-10-25 13:08:59 +0530

    Update main.tf

--- serverfiles/main.tf
+++ serverfiles/main.tf
@@ -1,26 +1,29 @@
-
 resource "aws_instance" "test_server" {
   ami           = "ami-0287a05f0ef0e9d9a"
   instance_type = "t2.micro"
   key_name      = "finance"
   vpc_security_group_ids = ["sg-0e7b3e09e354d5259"]
-  connection {
-     type = "ssh"
-     user = "ubuntu"
-     private_key = file("./finance.pem")
-     host = self.public_ip
-}
 
-provisioner "remote-exec" {
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("./finance.pem")
+    host        = self.public_ip
+  }
+
+  provisioner "remote-exec" {
     inline = ["echo 'wait to start the instance'"]
-}
-tags = {
+  }
+
+  tags = {
     name = "test-server"
-}
-provisioner "local-exec" {
-    command = "echo ${aws_instance.test-server.public_ip} > inventory "
-}
-provisioner "local-exec" {
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.test_server.public_ip} > inventory"
+  }
+
+  provisioner "local-exec" {
     command = "ansible-playbook /var/lib/jenkins/workspace/financeME/serverfiles/finance-playbook.yml"
-    }
+  }
 }
