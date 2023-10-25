2bd038254233059df348920e5df1cc5cba2beb4a
B. Lokanath prasad das <45465660+loknathD@users.noreply.github.com>
2023-10-25 12:19:17 +0530

    Create main.tf

---
+++ serverfiles/main.tf
@@ -0,0 +1,26 @@
+
+resource "aws_instance" "test_server" {
+  ami           = "ami-0287a05f0ef0e9d9a"
+  instance_type = "t2.micro"
+  key_name      = "finance"
+  vpc_security_group_ids = ["sg-0e7b3e09e354d5259"]
+  connection {
+     type = "ssh"
+     user = "ubuntu"
+     private_key = file("./finance.pem")
+     host = self.public_ip
+}
+
+provisinor "remote-exec" {
+    inline = ["echo 'wait to start the instance'"]
+}
+tags = {
+    name = "test-server"
+}
+provisinor "local-exec" {
+    inline = "echo ${aws_instance.test-server.public_ip} > inventory "
+}
+provisinor "local-exec" {
+    command = "ansible-playbook /var/lib/jenkins/workspace/financeME/serverfiles/finance-playbook.yml"
+    }
+}
